0c722e34e86497c10f28e65c6033e2d898f7d35b
Hanan Mohamud <152763595+hananmyy@users.noreply.github.com>
2024-11-08 16:54:55 +0300

    Add files via upload
    assignment done

---
+++ user_input.py
@@ -0,0 +1,15 @@
+# question 1
+
+name = input("Please enter your name: ")
+
+# question 2
+
+age = input("Please enter your age: ")
+
+# question 3
+
+location = input("Please enter your location: ")
+
+# question 4
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
